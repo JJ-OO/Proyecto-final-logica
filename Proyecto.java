3ef3abd934a43825087577996737b07e2d327960
LazyOne02 <77749125+LazyOne02@users.noreply.github.com>
2021-04-25 16:58:33 -0500

    v1 Programa base

--- Proyecto.java
+++ Proyecto.java
@@ -1,10 +1,11 @@
 public class Proyecto {
 
-	public static int aleatorio(n){
-		return Math.floor(Math.random()*n)+1;
+	public static int aleatorio(int n){
+		return (int)Math.round(Math.floor(Math.random()*n)+1);
 	}
 
 	public static void menu(){
+		
 		int opc;
 		do{
 
@@ -15,7 +16,7 @@ public class Proyecto {
 		System.out.println("3. Multiplicar");
 		System.out.println("4. Salir");
 		opc = 3;
-	}while(opc!=4)
+	}while(opc!=4);
 
 	System.out.println(aleatorio(4));
 
